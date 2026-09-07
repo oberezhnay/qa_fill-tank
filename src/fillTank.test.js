92fb752dbd5759f15296df10f839321dda641b8c
oberezhnay <oberezhna.o.v@gmail.com>
2026-09-07 17:26:00 +0300

    Add tests for round for function fillTank'

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -9,6 +9,7 @@ describe('fillTank', () => {
 
   it('should fill the tank to full capacity if no amount is specified', () => {
     const customer = createNewCustomer();
+
     fillTank(customer, 50);
     expect(customer.vehicle.fuelRemains).toBe(customer.vehicle.maxTankCapacity);
   });
@@ -18,12 +19,17 @@ describe('fillTank', () => {
     const fuelPrice = 100;
     const customerMoneyBeforePay = customer.money;
     const customerFuelRemainsBeforePay = customer.vehicle.fuelRemains;
+
     fillTank(customer, fuelPrice);
-    expect(customer.vehicle.fuelRemains).toBe(customerFuelRemainsBeforePay + customerMoneyBeforePay / fuelPrice);
+
+    expect(customer.vehicle.fuelRemains).toBe(
+      customerFuelRemainsBeforePay + customerMoneyBeforePay / fuelPrice,
+    );
   });
 
   it('should fill the possible tank value', () => {
     const customer = createNewCustomer();
+
     fillTank(customer, 50, 100);
     expect(customer.vehicle.fuelRemains).toBe(customer.vehicle.maxTankCapacity);
   });
@@ -31,8 +37,30 @@ describe('fillTank', () => {
   it('should not fill the fuel less then 2 liters', () => {
     const customer = createNewCustomer();
     const customerFuelRemainsBeforePay = customer.vehicle.fuelRemains;
+
     fillTank(customer, 100, 1);
     expect(customer.vehicle.fuelRemains).toBe(customerFuelRemainsBeforePay);
+  });
+
+  it('should round fuelRemains', () => {
+    const customer = createNewCustomer();
+
+    fillTank(customer, 50, 12.78);
+    expect(customer.vehicle.fuelRemains).toBe(20.7);
+  });
+
+  it('should round fuelRemains to one decimal place', () => {
+    const customer = createNewCustomer();
+
+    fillTank(customer, 50, 12.7);
+    expect(customer.vehicle.fuelRemains).toBe(20.7);
+  });
+
+  it('should round price', () => {
+    const customer = createNewCustomer();
+
+    fillTank(customer, 2.345, 2.1);
+    expect(customer.money).toBe(2995.08);
   });
 });
 
@@ -42,7 +70,6 @@ function createNewCustomer() {
     vehicle: {
       maxTankCapacity: 40,
       fuelRemains: 8,
-    }
+    },
   }
 }
-
