53489463da27b341f5cdffe9aba62ae37adf9655
oberezhnay <oberezhna.o.v@gmail.com>
2026-09-07 16:49:48 +0300

    Add tests for function fillTank'

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,9 +1,48 @@
 'use strict';
 
 describe('fillTank', () => {
-  // const { fillTank } = require('./fillTank');
+  const { fillTank } = require('./fillTank');
 
-  it('should ', () => {});
+  it('should be a function', () => {
+    expect(typeof fillTank).toBe('function');
+  });
 
-  // write tests here
+  it('should fill the tank to full capacity if no amount is specified', () => {
+    const customer = createNewCustomer();
+    fillTank(customer, 50);
+    expect(customer.vehicle.fuelRemains).toBe(customer.vehicle.maxTankCapacity);
+  });
+
+  it('should fill the tank how it possible if not enough money', () => {
+    const customer = createNewCustomer();
+    const fuelPrice = 100;
+    const customerMoneyBeforePay = customer.money;
+    const customerFuelRemainsBeforePay = customer.vehicle.fuelRemains;
+    fillTank(customer, fuelPrice);
+    expect(customer.vehicle.fuelRemains).toBe(customerFuelRemainsBeforePay + customerMoneyBeforePay / fuelPrice);
+  });
+
+  it('should fill the possible tank value', () => {
+    const customer = createNewCustomer();
+    fillTank(customer, 50, 100);
+    expect(customer.vehicle.fuelRemains).toBe(customer.vehicle.maxTankCapacity);
+  });
+
+  it('should not fill the fuel less then 2 liters', () => {
+    const customer = createNewCustomer();
+    const customerFuelRemainsBeforePay = customer.vehicle.fuelRemains;
+    fillTank(customer, 100, 1);
+    expect(customer.vehicle.fuelRemains).toBe(customerFuelRemainsBeforePay);
+  });
 });
+
+function createNewCustomer() {
+  return {
+    money: 3000,
+    vehicle: {
+      maxTankCapacity: 40,
+      fuelRemains: 8,
+    }
+  }
+}
+
